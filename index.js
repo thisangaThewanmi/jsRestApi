575bce6ed33240c86d38a4c2c06f4cfbac87ad1a
thisangaThewanmi <thisangat2005@gmail.com>
2024-11-04 10:08:17 +0530

    CRUD Operations

--- index.js
+++ index.js
@@ -25,3 +25,36 @@ app.get('/api/books', (req, res) => {
     res.json(books);
 });
 
+
+//POST
+app.post('/api/books', (req, res) => {
+    const newBook = {
+        id: books.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+    };
+    books.push(newBook);
+    res.status(201).json(newBook);
+});
+
+
+
+//UPDATE
+app.put('/api/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send('Book not found');
+
+    book.title = req.body.title;
+    book.author = req.body.author;
+    res.json(book);
+});
+
+//DELETE
+
+app.delete('/api/books/:id', (req, res) => {
+    const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
+    if (bookIndex === -1) return res.status(404).send('Book not found');
+
+    const deletedBook = books.splice(bookIndex, 1);
+    res.json(deletedBook);
+});
